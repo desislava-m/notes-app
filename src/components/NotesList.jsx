33a25f207c1c22f14ff83f4be336fd8ae89331b9
Dessie <desislava.m.alexandrova@gmail.com>
2025-07-25 20:56:12 +0300

    Adding remove function

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -3,12 +3,19 @@ import { notesContextObj } from "./NotesContext";
 
 export default function NotesList() {
 
-    const { notes } = useContext(notesContextObj);
+    const { notes, setNotes } = useContext(notesContextObj);
+
+    function handleRemove(note) {
+        setNotes((prev) => prev.filter((currentNote) => currentNote !== note))
+    }
 
     return (
         <>
             <ul>
-                {notes.map((note, index) => <li key={index}>{note}</li>)}
+                {notes.map((note, index) => 
+                <li key={index}>{note}{' '}
+                    <button onClick={() => handleRemove(note)}>Remove</button>
+                </li>)}
             </ul>
         </>
     )
