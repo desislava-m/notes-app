cdc885c381a3582c559984530387fb73e43839ad
Dessie <desislava.m.alexandrova@gmail.com>
2025-07-28 15:20:38 +0300

    Adding explanatory comments on saving data in localStorage

--- src/components/NotesContext.jsx
+++ src/components/NotesContext.jsx
@@ -1,17 +1,24 @@
 import { createContext } from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const notesContextObj = createContext();
 
 
 export default function NotesProvider({ children }) {
+  // Only on the first render because of the arrow it checks if we have anything in the storage, if not it sets notes to []
+  const [notes, setNotes] = useState(() => {
+    const storedNotes = localStorage.getItem("my-notes");
+    return storedNotes ? JSON.parse(storedNotes) : [];
+  });
 
+  // On the firt render meaning on [] it saves to the storage and every time notes changes it saves the string version to the localStorage
+  useEffect(() => {
+    localStorage.setItem("my-notes", JSON.stringify(notes));
+  }, [notes]);
 
-    const [ notes, setNotes ] = useState([])
-
-    return (
-        <notesContextObj.Provider value = {{ notes, setNotes }}>
-            {children}
-        </notesContextObj.Provider>
-    )
+  return (
+    <notesContextObj.Provider value={{ notes, setNotes }}>
+      {children}
+    </notesContextObj.Provider>
+  );
 }
